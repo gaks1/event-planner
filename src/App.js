949f909f2afd55f060e14c57c31ea135b04e899a
gaks1 <iwanyoike65@gmail.com>
2024-07-16 13:41:06 +0300

    Add a login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Home from './components/Home';
 
 const  App = () =>{
   return (
     <>
+    <Home />
     <h1>Home</h1>
     </>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+const Login = () => {
+    return(
+        <>
+        <form>
+            <h2>Login</h2>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+        </form>
+        </>
+    );
+}
+
+export default Login;
